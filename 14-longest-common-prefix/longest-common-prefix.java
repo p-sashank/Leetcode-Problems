3348144b57de1e399bec1c8984f37234d8c2c505
Perabathula Sashank Rao <sashank.perabathula@gmail.com>
2026-09-11 23:01:09 +0530

    Time: 1 ms (60.11%) | Memory: 43.4 MB (16.55%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int i=0; i<prefix.length();i++){
+            for(int j=1; j<strs.length; j++){
+                if(i>=strs[j].length() || prefix.charAt(i) != strs[j].charAt(i)){
+                    return prefix.substring(0,i);
+
+                }
+            }
+
+        }
+        return prefix;
+    }
+}
